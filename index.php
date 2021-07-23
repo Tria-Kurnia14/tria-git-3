719c7d2b4f4914db7dae35d4c9b706ba9561daf4
Tria-Kurnia14 <tria.kurnianingsih@gmail.com>
2021-07-23 23:47:06 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello Gaess!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident architecto ad facilis iure praesentium. Sequi pariatur exercitationem vel iusto unde quam vero assumenda, fugit mollitia nobis porro odit quis magnam.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
